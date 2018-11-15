1cf145309780d99d87a8202188bf89840f87ae3f
Shawn <mcpoyl93@students.rowan.edu>
2018-11-14 19:33:10 -0500

    Updated

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,9 +3,31 @@
 
 using namespace std;
 
-int main() {
+string passAttempt, answer;
+string staffPass = "pineapple";
 
-	
+int main() {
+	cout << "Welcome to the Rowan Mental Institution Hospital App. What would you like to do?" << endl;
+	cout << "Please input the number of your selection." << endl;
+	cout << "1. Staff Login" << endl << "2. Guest Functions" << endl;
+	cin >> answer;
+	if (answer == "1") {
+		cout << "Please input the staff password." << endl;
+		cin >> passAttempt;
+		if (passAttempt == staffPass) {
+			cout << "Access Granted" << endl;
+		}
+		else {
+			cout << "Access Denied" << endl;	
+		}
+		
+	}
+	else if (answer == "2") {
+
+	}
+	else {
+		cout << "That is not a valid answer. Please input a valid answer." << endl;
+	}
 
 	system("pause");
 	return 0;
